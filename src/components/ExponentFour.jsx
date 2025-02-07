30d1d1d7fc81338c929387fd7206eb7a05f9fdcd
Robert Muresan <robotzelu89@gmail.com>
2025-02-07 12:31:27 +0000

    done2

--- src/components/ExponentFour.jsx
+++
@@ -1,8 +0,0 @@
-const ExponentFour = ({count}) => (
-   <div className="exponent-counter-container">
-    <p className="exponent-label">{count}</p>
-    <p className="exponent-result">{count} * 2 * 2 * 2 = <span className="total">{count * Math.pow(count, 3)}</span></p>
-  </div>
-);
-
-export default ExponentFour;
--- src/components/ExponentSix.jsx
+++
@@ -1,8 +0,0 @@
-const ExponentSix = ({count}) => (
-  <div className="exponent-counter-container">
-    <p className="exponent-label">{count}</p>
-    <p className="exponent-result">{count} * 2 * 2 * 2 * 2 * 2 = <span className="total">{count * Math.pow(count, 5)}</span></p>
-  </div>
-);
-
-export default ExponentSix;
